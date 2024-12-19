466ca933f21909c8288660e9c630c86b80a2434e
Gokul Raj V <gokulrajv108@gmail.com>
2024-12-19 20:05:53 +0530

    Proper new lines between steps

--- src/libbdz/ui/cli.py
+++ src/libbdz/ui/cli.py
@@ -10,36 +10,63 @@ from rich import table
 _console = console.Console()
 
 
+def _get_colored_text(text: str, color_code: int) -> str:
+    color_key = ""
+    match color_code:
+        case 1:
+            color_key = "[green]"
+        case 2:
+            color_key = "[yellow]"
+        case 3:
+            color_key = "[cyan]"
+        case 4:
+            color_key = "[red]"
+
+    if color_key == "":
+        return text
+    else:
+        return color_key + text + "[/]"
+
+
+def _console_print(object_to_print, color_code: int = 0):
+    _console.print()
+
+    if isinstance(object_to_print, str):
+        object_to_print = _get_colored_text(object_to_print, color_code)
+
+    _console.print(object_to_print, highlight=False)
+
+
 def cinput(prompt: str) -> str:
-    input_val = _console.input("[bold green]" + prompt + "[/]")
+    input_val = _console.input(_get_colored_text("\n" + prompt, 1))
     return input_val
 
 
 def cprint(message: str):
-    _console.print(message, style="yellow")
+    _console_print(message, 2)
 
 
 def cprint_menu(menu_name: str, menu_list: list[str], menu_executables: list):
     while True:
-        _console.print(menu_name, style="cyan")
+        _console_print(menu_name, 3)
         for id, menu in enumerate(menu_list, start=1):
-            _console.print(str(id) + ". " + menu, style="cyan")
+            _console_print(str(id) + ". " + menu, 3)
 
         try:
             option = int(cinput("Enter your option (Ctrl-C to quit at any moment) : "))
         except KeyboardInterrupt:
             break
         except ValueError:
-            cprint("Invalid option")
+            _console_print("Invalid option", 2)
             continue
 
         if option > len(menu_list):
-            cprint("Invalid option")
+            _console_print("Invalid option", 2)
         else:
             try:
                 menu_executables[option - 1]()
             except KeyboardInterrupt:
-                cprint("Keyboard interrupt occured")
+                _console_print("Keyboard interrupt occured", 2)
 
 
 def cprint_table(
@@ -55,7 +82,7 @@ def cprint_table(
     for row in rows:
         table_to_display.add_row(*row)
 
-    _console.print(table_to_display)
+    _console_print(table_to_display)
 
 
 class LiveStatusUpdate:
